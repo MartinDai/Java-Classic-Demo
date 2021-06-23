dbbe5234d0b46bc9540d4c6335c7fc90651e42e0
daixiaoming <jingang.dxm@raycloud.com>
2021-06-23 16:41:20 +0800

    添加输出

--- src/main/java/com/doodl6/demo/jvm/MetaspaceOOM.java
+++ src/main/java/com/doodl6/demo/jvm/MetaspaceOOM.java
@@ -9,15 +9,17 @@ import net.sf.cglib.proxy.MethodInterceptor;
 public class MetaspaceOOM {
 
     /**
-     * JVM参数：-XX:MaxMetaspaceSize=5M -XX:+HeapDumpOnOutOfMemoryError
+     * JVM参数：-XX:MaxMetaspaceSize=10M -XX:+HeapDumpOnOutOfMemoryError
      */
     public static void main(String[] args) {
+        int loadClassCount = 0;
         while (true) {
             Enhancer enhancer = new Enhancer();
             enhancer.setSuperclass(Object.class);
             enhancer.setUseCache(false);
             enhancer.setCallback((MethodInterceptor) (obj, method, args1, proxy) -> proxy.invokeSuper(obj, args1));
             enhancer.create();
+            System.out.println("已加载增强类数量:" + (++loadClassCount));
         }
     }
 
